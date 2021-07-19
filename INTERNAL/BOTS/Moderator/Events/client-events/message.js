e28bf23e102911baff2490b887e2a7155d4b1ed1
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-19 15:11:43 +0300

    Update message.js

--- INTERNAL/BOTS/Moderator/Events/client-events/message.js
+++ INTERNAL/BOTS/Moderator/Events/client-events/message.js
@@ -144,51 +144,5 @@ module.exports = class {
                 }
             }
         }
-        if (!message.content.startsWith(client.config.prefix)) return;
-        if (message.author.bot) return;
-        let command = message.content.split(' ')[0].slice(client.config.prefix.length);
-        let cmd;
-        let args = message.content.split(' ').slice(1);
-        if (client.commands.has(command)) {
-            cmd = client.commands.get(command);
-        } else if (client.aliases.has(command)) {
-            cmd = client.commands.get(client.aliases.get(command));
-        } else return;
-        const embed = new Discord.MessageEmbed();
-        if (!cmd.config.enabled) return;
-        if (cmd.config.dmCmd && (message.channel.type !== 'dm')) return message.channel.send(`${emojis.get("dmcmd").value()} Bu komut bir **DM** komutudur.`);
-        if (cmd.config.ownerOnly && (message.author.id !== client.config.owner)) return message.channel.send(`${emojis.get("tantus").value()} Bu komutu sadece ${client.owner} kullanabilir.`);
-        if (cmd.config.onTest && !utils.get("testers").value().includes(message.author.id) && (message.author.id !== client.config.owner)) return message.channel.send(`${emojis.get("ontest").value()} Bu komut henüz **test aşamasındadır**.`);
-        if (cmd.config.rootOnly && !utils.get("mod").value().includes(message.author.id) && (message.author.id !== client.config.owner)) return message.channel.send(`${emojis.get("rootonly").value()} Bu komutu sadece **yardımcılar** kullanabilir.`);
-        if (cmd.config.adminOnly && !message.member.permissions.has("MANAGE_ROLES") && (message.author.id !== client.config.owner)) return message.channel.send(`${emojis.get("moddonly").value()} Bu komutu sadece **yöneticiler** kullanabilir.`);
-        if (cmd.info.cmdChannel & message.guild && message.guild.channels.cache.get(channels.get(cmd.info.cmdChannel).value()) && (message.channel.id !== channels.get(cmd.info.cmdChannel).value())) return message.channel.send(`${emojis.get("text").value()} Bu komutu ${message.guild.channels.cache.get(channels.get(cmd.info.cmdChannel).value())} kanalında kullanmayı dene!`);
-        if (message.guild && !cmd.config.dmCmd) {
-            const requiredRoles = cmd.info.accaptedPerms || [];
-            let allowedRoles = [];
-            await requiredRoles.forEach(rolValue => {
-                allowedRoles.push(message.guild.roles.cache.get(roles.get(rolValue).value()));
-            });
-            let deyim = `Bu komutu kullanabilmek için ${allowedRoles[0]} rolüne sahip olmalısın!`;
-            if (allowedRoles.length > 1) deyim = `Bu komutu kollanabilmek için aşağıdaki rollerden birisine sahip olmalısın:\n${allowedRoles.join(`\n`)}`;
-            if ((allowedRoles.length >= 1) && !allowedRoles.some(role => message.member.roles.cache.has(role.id)) && !message.member.permissions.has("ADMINISTRATOR") && (message.author.id !== client.config.owner)) {
-                return message.channel.send(embed.setDescription(deyim).setColor('#2f3136'));
-            }
-        }
-        let uCooldown = client.cmdCoodown[message.author.id];
-        if (!uCooldown) {
-            client.cmdCoodown[message.author.id] = {};
-            uCooldown = client.cmdCoodown[message.author.id];
-        }
-        let time = uCooldown[cmd.info.name] || 0;
-        if (time && (time > Date.now())) return message.channel.send(`${emojis.get("dmcmd").value()} Komutu tekrar kullanabilmek için lütfen **${Math.ceil((time - Date.now()) / 1000)}** saniye bekle!`);
-
-        client.logger.log(`[(${message.author.id})] ${message.author.username} ran command [${cmd.info.name}]`, "cmd");
-        if ((command !== 'tag') && (message.channel.id === "857659757233700875") && !message.member.permissions.has("MANAGE_ROLES")) return;
-        try {
-            cmd.run(client, message, args);
-        } catch (e) {
-            console.log(e);
-            return message.channel.send(`${emojis.get("error").value()} | Sanırım bir hata oluştu...`);
-        }
     }
 }
