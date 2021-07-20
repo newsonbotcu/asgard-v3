e8c6a0ff6c4bedd0e8ad72bc987b3ca90b38118c
Taurusian Tantoony <tantoony.id@gmail.com>
2021-07-20 04:42:02 +0300

    Update task_excuse.js

--- INTERNAL/BOTS/Moderator/Commands/components/Tasks/task_excuse.js
+++ INTERNAL/BOTS/Moderator/Commands/components/Tasks/task_excuse.js
@@ -2,12 +2,14 @@ const Component = require("../../../Base/Component");
 const Discord = require('discord.js');
 const low = require('lowdb');
 const Task_profile = require("../../../../../MODELS/Economy/Task_profile");
+const { sayi, checkDays } = require("../../../../../HELPERS/functions");
+const { stripIndent } = require("common-tags");
 class RolSeçim extends Component {
     constructor(client) {
         super(client, {
             name: "task_excuse",
             accaptedPerms: [],
-            cooldown: 10000,
+            cooldown: 180000,
             enabled: true,
             ownerOnly: false,
             rootOnly: false,
@@ -26,13 +28,16 @@ class RolSeçim extends Component {
         const mentioned = guild.members.cache.get(ctx.user.id);
         const profiles = await Task_profile.find();
         const excuseCount = profiles.map(p => p.excuses.length).reduce((a, c) => a + c, 0);
-        const parent = guild.channels.cache.get(channels.get("excuse").value());
-
+        const mazeretlog = guild.channels.cache.get(channels.get("excuse").value());
+        const myProfile = await Task_profile.findOne({ _id: mentioned.user.id });
+        if (myProfile.excuses.filter(ex => checkDays(ex.created) < 15).length > 1) return await ctx.send(`İzin sınırını aştın!`, {
+            ephemeral: true
+        });
         const channel = await guild.channels.create(`mazeret-${excuseCount + 1}`, {
             type: 'text',
             topic: `${mentioned.displayName} (${mentioned.roles.highest})`,
             nsfw: false,
-            parent: parent
+            parent: mazeretlog.parentID
         });
         await channel.updateOverwrite(mentioned.user.id, {
             VIEW_CHANNEL: true
@@ -41,11 +46,48 @@ class RolSeçim extends Component {
         const collector_1 = new Discord.MessageCollector(channel, filter_1, {
             time: 120000
         });
-        let reason;
-        let days;
+        let reeson;
         await channel.send(`Selam ${mentioned}!, mazeret sebebini öğrenebilir miyim?`);
-        collector_1.on("collect", async (message) => {
-
+        collector_1.on("collect", (message) => {
+            reeson = message.content;
+            collector_1.stop("finished");
+        });
+        collector_1.on("end", async (collected, reason) => {
+            if (reason !== "finished") {
+                await channel.send("Bu işlem zaman aşımına uğradı!");
+                setTimeout(async () => {
+                    await channel.delete();
+                }, 5000);
+                return;
+            } else {
+                await channel.send(`Kaç gün sonra tekrardan aktif olacağını yazar mısın?`);
+                const collector_2 = new Discord.MessageCollector(channel, filter_1, {
+                    time: 12000
+                });
+                collector_2.on("collect", (message) => {
+                    if (!sayi(message.comtent)) return await channel.send("Bir sayı girmelisin!");
+                    if (Number(message.content) > 3) return await message.channel.send("Üç günden fazla sürecek bir izin almak için **Loki** permine sahip insanlara ulaş.");
+                    await Task_profile.updateOne({ _id: mentioned.user.id }, {
+                        $push: {
+                            excuses: {
+                                created: new Date(),
+                                duration: message.content,
+                                reason: reeson
+                            }
+                        }
+                    });
+                    collector_2.stop();
+                    await channel.send("Mazeretin başarıyla oluşturuldu!");
+                    setTimeout(async () => {
+                        await channel.delete();
+                    }, 5000);
+                    await mazeretlog.send(new Discord.MessageEmbed().setDescription(stripIndent`
+                    Kulllanıcı: ${mentioned}
+                    Sebep: ${reeson}
+                    Süre: ${message.content} gün
+                    `));
+                });
+            }
         });
 
 
