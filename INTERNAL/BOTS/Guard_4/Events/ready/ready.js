ff3e6543464f5d3a506ebb0ee08a91316ef36337
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-27 22:41:42 +0300

    Update ready.js

--- INTERNAL/BOTS/Guard_4/Events/ready/ready.js
+++ INTERNAL/BOTS/Guard_4/Events/ready/ready.js
@@ -6,7 +6,7 @@ class Ready {
 
     async run(client) {
         client = this.client;
-        const guild = client.guilds.cache.get(client.config.server);
+        client.guild = client.guilds.cache.get(client.config.server);
         client.logger.log(`${client.user.tag}, ${client.users.cache.size} kişi için hizmet vermeye hazır!`, "ready");
         client.user.setPresence({ activity: client.config.status, status: "idle" });
         client.owner = client.users.cache.get(client.config.owner);
