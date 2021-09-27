eaba686fe719f74f5b0bc79efa2868c4898c4284
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-27 22:27:42 +0300

    Update memberXp.js

--- INTERNAL/EVENTS/memberXp.js
+++ INTERNAL/EVENTS/memberXp.js
@@ -16,11 +16,10 @@ class MemberXpEvent {
         const channels = await low(client.adapters('channels'));
         const profile = await VoiceRecords.findOne({ _id: member.user.id });
         const ranks = await roleXp.find();
-        const myXp = profile.records.map(p => p.xp).reduce((a, c) => a + ca, 0);
+        const myXp = profile.records.map(p => p.xp).reduce((a, c) => a + c, 0);
         const myRank = ranks.find(rank => member.roles.cache.has(rank));
         //console.log(ranks.sort((a, b) => b.requiredXp - a.requiredXp).map(r => member.guild.roles.cache.get(r._id).name));
-        const nextRank = ranks.sort((a, b) => b.requiredXp - a.requiredXp).find(rank => rank.requiredXp > (myRank ? myRank.requiredXp : 0));
-        console.log(nextRank);
+        const nextRank = ranks.sort((a, b) => a.requiredXp - b.requiredXp).find(rank => rank.requiredXp > (myRank ? myRank.requiredXp : 0));
         if (myXp > nextRank.requiredXp) {
             await member.roles.add(nextRank._id);
             await member.roles.remove(myRank._id);
