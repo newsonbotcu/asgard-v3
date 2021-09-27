85d719d1151a92ac1d2e7ea13ed114ef10786274
Taurusian Tantoony <tantoony.id@gmail.com>
2021-09-27 14:50:02 +0300

    Update stat.js

--- INTERNAL/BOTS/Moderator/Commands/slash-commands/Stat/stat.js
+++ INTERNAL/BOTS/Moderator/Commands/slash-commands/Stat/stat.js
@@ -125,6 +125,7 @@ module.exports = class HelloCommand extends SlashCommand {
                 Yayın Açık: \`${Math.floor(records.filter(r => r.streaming).map(r => r.duration).length > 0 ? records.filter(r => r.streaming).map(r => r.duration).reduce((a, b) => a + b, 0) / 60000 : 0)} dakika\`
                 Kamera Açık: \`${Math.floor(records.filter(r => r.videoOn).map(r => r.duration).length > 0 ? records.filter(r => r.streaming).map(r => r.duration).reduce((a, b) => a + b, 0) / 60000 : 0)} dakika\`
                 ${!mentioned.roles.cache.has(roles.get("cmd-crew").value()) ? "" : stripIndent`
+                
                 __**Yetki Atlama Durumu**__
                 ${bar(profile ? profile.xp : 0, nextRank.requiredXp)}`}
                 `).setThumbnail(mentioned.user.displayAvatarURL({ type: 'gif' })).setColor(mentioned.displayHexColor).setTitle(guild.name);
